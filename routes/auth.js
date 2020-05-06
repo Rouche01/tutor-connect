1ff59555d8c27f16c28c645c3fd2bea3f21b724c
Richard Emate <richardemate@gmail.com>
2020-05-06 04:01:35 +0100

    creating users login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,34 @@
 const router = require('express').Router();
-const { register } = require('../controllers/auth');
+const { register, login, createAdmin } = require('../controllers/auth');
 
+
+// Creating the root admin
+createAdmin();
 
 // Students registration route
 router.post('/register-student', async (req, res) => {
     await register(req, res, "student");
 });
 
-//Tutor registration route
+// Tutor registration route
 router.post('/register-tutor', async(req, res) => {
     await register(req, res, "tutor");
-})
+});
+
+
+// Students login route
+router.post('/login-student', async(req, res) => {
+    await login(req, res, "student");
+});
+
+// Tutors login route
+router.post('/login-tutor', async(req, res) => {
+    await login(req, res, "tutor");
+});
+
+// Admin login route
+router.post('/login-admin', async(req, res) => {
+    await login(req, res, "admin");
+});
 
 module.exports = router;
